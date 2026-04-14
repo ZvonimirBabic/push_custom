82c6e377888dddefc99e38ac6723dbce0f8b285b
ZvonimirBabic <zvobabic@hotmail.com>
2026-04-14 20:34:38 +0200

    *migration

--- android/src/main/kotlin/uk/orth/push/MessagingService.kt
+++ android/src/main/kotlin/uk/orth/push/MessagingService.kt
@@ -2,20 +2,12 @@ package uk.orth.push
 
 import android.app.ActivityManager
 import android.app.ActivityManager.RunningAppProcessInfo
-import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
-import android.util.Log
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-import uk.orth.push.PushHostHandlers.Companion.ON_BACKGROUND_MESSAGE_PROCESSING_COMPLETE
 
 class MessagingService : FirebaseMessagingService() {
-    private var asyncProcessingPendingResult: BroadcastReceiver.PendingResult? = null
-    private var flutterBackgroundMessageProcessingCompleteReceiver: FlutterBackgroundMessageProcessingCompleteReceiver? = null
-
     override fun onNewToken(registrationToken: String) {
         super.onNewToken(registrationToken)
         PushPlugin.onNewToken(this, registrationToken)
@@ -33,23 +25,9 @@ class MessagingService : FirebaseMessagingService() {
         }
         intent.putExtras(remoteMessage.toIntent().extras ?: android.os.Bundle())
 
-        listenForFlutterApplicationToFinishProcessingMessage()
         sendMessageToFlutterApplication(intent)
     }
 
-    /**
-     * Launches a broadcast receiver ([FlutterBackgroundMessageProcessingCompleteReceiver]) so that
-     * this service can be informed when the Flutter application completes processing.
-     *
-     * goAsync() also increases the execution time from 10s/20s (depending on API level) to 30s
-     */
-    private fun listenForFlutterApplicationToFinishProcessingMessage() {
-        if (flutterBackgroundMessageProcessingCompleteReceiver == null) {
-            flutterBackgroundMessageProcessingCompleteReceiver =
-                FlutterBackgroundMessageProcessingCompleteReceiver(this)
-            asyncProcessingPendingResult = goAsync()
-        }
-    }
 
     private fun sendMessageToFlutterApplication(intent: Intent) {
         val isApplicationInForeground = isApplicationInForeground()
@@ -77,39 +55,6 @@ class MessagingService : FirebaseMessagingService() {
         return false
     }
 
-    /**
-     * A dynamic broadcast receiver registered to listen to a
-     * `PUSH_ON_BACKGROUND_MESSAGE_PROCESSING_COMPLETE`
-     */
-    private inner class FlutterBackgroundMessageProcessingCompleteReceiver(
-        context: Context,
-    ) : BroadcastReceiver() {
-        init {
-            val filter = IntentFilter()
-            filter.addAction(ON_BACKGROUND_MESSAGE_PROCESSING_COMPLETE)
-            LocalBroadcastManager.getInstance(context).registerReceiver(this, filter)
-        }
-
-        override fun onReceive(
-            context: Context,
-            intent: Intent,
-        ) {
-            flutterBackgroundMessageProcessingCompleteReceiver?.let {
-                LocalBroadcastManager.getInstance(context).unregisterReceiver(it)
-                flutterBackgroundMessageProcessingCompleteReceiver = null
-            }
-            if (intent.action == ON_BACKGROUND_MESSAGE_PROCESSING_COMPLETE) {
-                finish()
-            } else {
-                Log.e(TAG, String.format("Received unknown intent action: %s", intent.action))
-            }
-        }
-    }
-
-    private fun finish() {
-        asyncProcessingPendingResult?.finish()
-        asyncProcessingPendingResult = null
-    }
 
     companion object {
         private val TAG = MessagingService::class.qualifiedName
