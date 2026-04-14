d5cc9ff775c6227315eca60e5e282e8bbbd9b644
ZvonimirBabic <zvobabic@hotmail.com>
2026-04-14 20:46:51 +0200

    *migration2

--- android/src/main/kotlin/uk/orth/push/BackgroundFlutterAppLauncher.kt
+++ android/src/main/kotlin/uk/orth/push/BackgroundFlutterAppLauncher.kt
@@ -2,6 +2,8 @@ package uk.orth.push
 
 import android.content.Context
 import android.content.Intent
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import com.google.firebase.messaging.RemoteMessage
 import io.flutter.embedding.engine.FlutterEngine
@@ -25,36 +27,49 @@ import uk.orth.push.serialization.PushHostApi
  * @param onComplete Callback to invoke when processing is complete
  */
 class BackgroundFlutterAppLauncher(
-    context: Context,
-    intent: Intent,
+    private val context: Context,
+    private val intent: Intent,
     private val onComplete: (() -> Unit)? = null,
 ) {
     private val remoteMessage: RemoteMessage = RemoteMessage((intent.extras)!!)
-    private val flutterEngine: FlutterEngine = FlutterEngine(context, null)
-    private var pushHostHandlers: PushHostHandlers = PushHostHandlers(context, flutterEngine.dartExecutor.binaryMessenger)
+    private var flutterEngine: FlutterEngine? = null
+    private var pushHostHandlers: PushHostHandlers? = null
 
     init {
-        // Setup listener for background processing
-        pushHostHandlers.setupForBackgroundNotificationProcessing(remoteMessage) { finish() }
-        // Setup listener
-        PushHostApi.setUp(flutterEngine.dartExecutor.binaryMessenger, pushHostHandlers)
-        // Launch the users app isolate manually
-        flutterEngine.dartExecutor.executeDartEntrypoint(DartExecutor.DartEntrypoint.createDefault())
-        // Even though lifecycle parameter is @NonNull, the implementation
-        // `FlutterEngineConnectionRegistry`
-        // does not use it, because it is a bug in the API design. See
-        // https://github.com/flutter/flutter/issues/90316
-//         flutterEngine.broadcastReceiverControlSurface.attachToBroadcastReceiver(broadcastReceiver, null)
+        // FlutterEngine initialization must happen on the main thread.
+        // This class may be called from Firebase-Messaging-Intent-Handle background thread,
+        // so we defer the initialization to the main thread using Handler.
+        Handler(Looper.getMainLooper()).post {
+            initializeFlutterEngine()
+        }
+    }
+
+    private fun initializeFlutterEngine() {
+        try {
+            flutterEngine = FlutterEngine(context, null)
+            pushHostHandlers = PushHostHandlers(context, flutterEngine!!.dartExecutor.binaryMessenger)
+
+            // Setup listener for background processing
+            pushHostHandlers!!.setupForBackgroundNotificationProcessing(remoteMessage) { finish() }
+            // Setup listener
+            PushHostApi.setUp(flutterEngine!!.dartExecutor.binaryMessenger, pushHostHandlers)
+            // Launch the users app isolate manually
+            flutterEngine!!.dartExecutor.executeDartEntrypoint(DartExecutor.DartEntrypoint.createDefault())
+        } catch (e: Exception) {
+            Log.e(TAG, "Failed to initialize Flutter engine", e)
+            onComplete?.invoke()
+        }
     }
 
     private fun finish() {
-//        flutterEngine.broadcastReceiverControlSurface.detachFromBroadcastReceiver()
         Log.i(
             TAG,
             "Manually launched Flutter application has finished processing message. " +
                 "Destroying FlutterEngine and finishing asynchronous MessagingService",
         )
-        flutterEngine.destroy()
+        flutterEngine?.destroy()
+        flutterEngine = null
+        pushHostHandlers = null
         onComplete?.invoke()
     }
 
