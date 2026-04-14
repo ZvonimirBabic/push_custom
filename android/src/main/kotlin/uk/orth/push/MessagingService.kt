9676f36e6274b4163c29721060d49a7d02ec5325
ZvonimirBabic <zvobabic@hotmail.com>
2026-04-14 19:48:06 +0200

    *replace BroadcastReceiverWith FirebaseMessagingService

--- android/src/main/kotlin/uk/orth/push/MessagingService.kt
+++ android/src/main/kotlin/uk/orth/push/MessagingService.kt
@@ -1,24 +1,61 @@
 package uk.orth.push
 
+import android.app.ActivityManager
+import android.content.Context
+import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
-// We listen to the message using a broadcast receiver (`FirebaseMessagingReceiver`)
-// instead of this/ a service because multiple broadcast listeners can be called
-// in response to an intent filter, but only 1 activity/ service can be called. This means
-// we won't conflict with other packages/ plugins and will be more reliable.
-// See `android:permission` on `<receiver>`
-// [docs](https://developer.android.com/guide/topics/manifest/receiver-element#prmsn) for more
-// information.
-// See https://github.com/firebase/snippets-android/blob/master/messaging/app/src/main/java/com/google/firebase/example/messaging/kotlin/MyFirebaseMessagingService.kt#L23-L49 for an example of onMessageReceived
 class MessagingService : FirebaseMessagingService() {
-    override fun onNewToken(registrationToken: String) {
-        super.onNewToken(registrationToken)
-        PushPlugin.onNewToken(this, registrationToken)
-    }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        // This class does not do anything in onMessageReceived, since [FirebaseMessagingReceiver]
-        // listens to `com.google.android.c2dm.intent.RECEIVE` declared in the manifest.
+        if (remoteMessage.data.isEmpty()) {
+            return
+        }
+
+        handleMessage(applicationContext, remoteMessage)
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        Log.d(TAG, "New token: $token")
+    }
+
+    private fun handleMessage(context: Context, remoteMessage: RemoteMessage) {
+        val data = remoteMessage.data
+
+        when {
+            isApplicationInForeground(context) -> {
+                PushHostHandlers.sendMessageToFlutterApp(context, data)
+            }
+
+            PushPlugin.isMainActivityRunning -> {
+                PushHostHandlers.sendBackgroundMessageToFlutterApp(context, data)
+            }
+
+            else -> {
+                BackgroundFlutterAppLauncher(context, data)
+            }
+        }
+    }
+
+    private fun isApplicationInForeground(context: Context): Boolean {
+        val activityManager =
+            context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+
+        val processes = activityManager.runningAppProcesses ?: return false
+
+        for (process in processes) {
+            if (process.importance ==
+                ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND
+            ) {
+                return true
+            }
+        }
+        return false
+    }
+
+    companion object {
+        private const val TAG = "MyFirebaseMsgService"
     }
 }
